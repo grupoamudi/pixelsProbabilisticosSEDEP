66899402a1ec58336938c9fba60e27d0c39b195b
Bernardo Camargos <ber.camargos@gmail.com>
2017-10-25 21:54:04 -0200

    - Added instrunctions and write to file

--- peopleCounter/reader.py
+++ peopleCounter/reader.py
@@ -3,11 +3,17 @@ import sys
 import struct
 import datetime
 
+if len(sys.argv) == 1:
+    print("Command should be 'python reader.py serialport'\n, where serialport is the port to communicate, for Windows would be something like COM4\n For linux something like /dev/ttyUSB0")
+    exit()
 serialport = sys.argv[1]
 ser = serial.Serial(serialport, 115200)
 while True:
     data = ser.read(2)
     nmacs = struct.unpack("<H", data)[0]
     line = str(datetime.datetime.now()) + ' ' + str(nmacs) + '\n'
+    f = open('counter.txt', 'a')
+    f.write(line)
+    f.close()
     print(line)
 
