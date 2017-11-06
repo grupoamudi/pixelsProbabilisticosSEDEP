f892a62384a3d5bc58442d6259cf56cfe9a98fa7
Bernardo Camargos <ber.camargos@gmail.com>
2017-11-05 22:42:52 -0200

    - Added support to python 2

--- peopleCounter/reader.py
+++ peopleCounter/reader.py
@@ -10,7 +10,9 @@ serialport = sys.argv[1]
 ser = serial.Serial(serialport, 115200, timeout=1)
 while True:
     data = ser.read(50)
-    if len(data) == 5 and data[0] == 48 and data[1] == 48 and data[2] == 48:
+    if (len(data) == 5 and data[0] == '0' and data[1] == '0' and data[2] == '0') or\
+       (len(data) == 5 and data[0] == 48  and data[1] == 48  and data[2] == 48 ):
+
         nmacs = struct.unpack("<H", data[3:])[0]
         line = str(datetime.datetime.now()) + ' ' + str(nmacs) + '\n'
         f = open('counter.txt', 'a')
@@ -20,4 +22,7 @@ while True:
         f.write(struct.pack("<H", nmacs))
         f.close()
         print(line)
+    else:
+        if len(data) == 5:
+            print(data[0])
 
